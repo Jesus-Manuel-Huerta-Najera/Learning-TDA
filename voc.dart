9ac009b68b8820548c62943afa1bd6577fbe473d
Jesus-Manuel-Huerta-Najera <50387895+Jesus-Manuel-Huerta-Najera@users.noreply.github.com>
2020-04-16 00:27:59 -0500

    V0.0.3

--- voc.dart
+++ voc.dart
@@ -33,6 +33,8 @@ class multi extends StatefulWidget{
 class _myHomePageState extends State<multi>{
   _myHomePageState(this.text);
   String text;
+  List<Color> _colors = [Colors.black54,Colors.black54];
+  List<double> _stops = [0.0,0.7];
   @override
   void initState() {
     super.initState();
@@ -41,23 +43,84 @@ class _myHomePageState extends State<multi>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      backgroundColor: Colors.cyan,
-      appBar: AppBar(title: Text("$text"),
-        backgroundColor: Colors.cyanAccent,
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(title: Text("A ESCRIBIR"),
+        backgroundColor: Colors.cyan,
+        automaticallyImplyLeading: false,
         centerTitle: true,
       ),
       body: ListView(
         children: <Widget>[
+      new Container(
+      padding: EdgeInsets.all(40.0),
+      child: new Column(
+        children: <Widget>[
+          new Padding(padding: EdgeInsets.only(top: 50.0)),
           new Text(
             "$text",
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 100.00,
+              fontSize: 150.00,
               fontWeight: FontWeight.bold
 
             ),
           ),
+          new Padding(padding: EdgeInsets.only(top: 150.0)),
+          new Row(
+            mainAxisAlignment:   MainAxisAlignment.center,
+            children: <Widget>[
+              new Container(
+                decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.pinkAccent,Colors.pinkAccent],
+                    stops: _stops),
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))
+                ),
+                // padding: EdgeInsets.all(30.00),
+                width: 120,
+                height: 70,
+                child: new MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+                  onPressed: () {
 
+                  },
+                  child: Text("Imagen",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
+                  ),
+                  color: Colors.cyanAccent,
+                ),
+              ),
+              new Padding(padding: EdgeInsets.all(20.0)),
+              new Container(
+                decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.pinkAccent,Colors.pinkAccent],
+                    stops: _stops),
+                    borderRadius: BorderRadius.all(Radius.circular(20.0))
+                ),
+                // padding: EdgeInsets.all(30.00),
+                width: 120,
+                height: 70,
+                child: new MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+
+                  onPressed: () {
+
+                  },
+                  child: Text("Foto",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 25
+                    ),
+                  ),
+                  color: Colors.cyanAccent,
+
+                ),
+              ),
+            ],
+          ),
+    ],
+    ),
+    ),
         ],
       ),
 
