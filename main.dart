ca8541625be17a0225cff9d7cd831172d5f0bece
Jesus-Manuel-Huerta-Najera <50387895+Jesus-Manuel-Huerta-Najera@users.noreply.github.com>
2020-04-16 00:20:54 -0500

    V0.0.3

--- main.dart
+++ main.dart
@@ -2,86 +2,134 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'Main_voc.dart';
+
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget{
+  AudioPlayer audioPlayer;
+  AudioCache audioCache;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.cyan,
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.green,
-      ),
-      home: homePage(),
+
+      home: homePage(this.audioPlayer,this.audioCache),
     );
   }
 
 }
 class homePage extends StatefulWidget{
+  homePage(this.audioPlayer,this.audioCache);
+  AudioPlayer audioPlayer;
+  AudioCache audioCache;
   @override
-  _myHomePageState createState() => new _myHomePageState();
+  _myHomePageState createState() => new _myHomePageState(this.audioPlayer,this.audioCache);
 
 
 }
 class _myHomePageState extends State<homePage>{
+  _myHomePageState(this.audioPlayer,this.audioCache);
+  AudioPlayer audioPlayer;
+  AudioCache audioCache;
+  List<Color> _colors = [Colors.deepOrangeAccent,Colors.green];
+  List<double> _stops = [0.0,0.7];
   @override
- // void initState() {
-   // super.initState();
-  //}
-  double buttonHigh = 50;
-  double buttonWidth = 100;
+  void initState() {
+    super.initState();
+    //audioPlayer = AudioPlayer();
+   // audioCache = AudioCache(fixedPlayer: audioPlayer);
+    audioPlayer.stop();
+    audioCache.play("pregunta.mp3");
+  }
+  double buttonHigh = 150;
+  double buttonWidth = 200;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
      resizeToAvoidBottomPadding : false,
-      backgroundColor: Colors.cyan,
-      appBar: AppBar(title: Text("Welcome"),
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(title: Text("Welcome"
+      ,style: TextStyle(
+          fontFamily: 'Schyler',
+          fontSize: 40.0,
+          //fontWeight: FontWeight.bold
+        ),
+      ),
         automaticallyImplyLeading: false,
-        backgroundColor: Colors.cyanAccent,
+        backgroundColor: Colors.cyan,
         centerTitle: true,
       ),
+
       body: ListView(
         children: <Widget>[
-           Container(
-            padding: EdgeInsets.all(30.00),
-            width: buttonWidth,
-            height: buttonHigh,
-            child: new MaterialButton(
-              child: Text("Vocales",
+          new Container(
+            padding: EdgeInsets.all(40.0),
+              child: new Column(
+              children: <Widget>[
+                new Padding(padding: EdgeInsets.only(top: 30.0)),
+               Container(
+               // padding: EdgeInsets.all(30.00),
+                 decoration: BoxDecoration(gradient: LinearGradient(colors: _colors,
+                 stops: _stops),
+                   borderRadius: BorderRadius.all(Radius.circular(20.0))
+                 ),
+                width: 200,
+                height: 150,
+                child: new MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+                  child: Text("Vocales",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-
+                    fontFamily: 'Schyler',
+                    fontSize: 35.0,
+                    //fontWeight: FontWeight.bold
                 ),),
               color: Colors.blue,
               onPressed: () {
+                audioPlayer.stop();
+                audioCache.play("vocales.mp3");
+                //audioPlayer.pause();
                 Navigator.push(context, MaterialPageRoute(
-                    builder: (context) => Main_voc()));
+                    builder: (context) => Main_voc(audioPlayer,audioCache)));
               },
             ),
           ),
+          new Padding(padding: EdgeInsets.only(top: 40.0)),
           new Container(
-            padding: EdgeInsets.all(30.00),
-            width: buttonWidth,
-            height: buttonHigh,
+            decoration: BoxDecoration(gradient: LinearGradient(colors: _colors,
+                stops: _stops),
+                borderRadius: BorderRadius.all(Radius.circular(20.0))
+            ),
+           // padding: EdgeInsets.all(30.00),
+            width: 200,
+            height: 150,
             child: new MaterialButton(
-                  onPressed: null,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40.0)),
+                  onPressed: () {
+                audioPlayer.stop();
+                    SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                  },
                child: Text("Salir",
                  textAlign: TextAlign.center,
                  style: TextStyle(
-
-                 ),),
-              color: Colors.blue,
-    )
+                     fontFamily: 'Schyler',
+                     fontSize: 45.0,
+                     //fontWeight: FontWeight.bold
+                 ),
+                 ),
+              color: Colors.blueGrey,
+    ),
           ),
-
+          ],
+        ),
+          ),
         ],
-      ),
+
+    ),
 
     );
   }
